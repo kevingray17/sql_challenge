b847182e4c96484bcc2a6a178984c1d45869c9ad
KG17 <kevingray17@gmail.com>
2020-11-09 09:49:25 -0500

    reordred q 4 by emp no and last name

--- EmployeeSQL/employee_SQL.sql
+++ EmployeeSQL/employee_SQL.sql
@@ -11,13 +11,11 @@ WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
 ORDER BY hire_date;
 
 -- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
--- SELECT e.emp_no, dm.dept_no, dt.dept_name, e.first_name, e.last_name
 SELECT dm.dept_no, dt.dept_name, e.emp_no, e.last_name, e.first_name
 FROM dept_manager_table dm
 INNER JOIN employees_table e ON dm.emp_no = e.emp_no
 INNER JOIN departments_table dt ON dt.dept_no = dm.dept_no 
 ORDER BY dm.dept_no ASC, e.emp_no ASC;
-
 
 -- List the  department of each employee with the following information: employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
@@ -26,6 +24,7 @@ INNER JOIN departments_table d ON
 de.dept_no=d.dept_no
 INNER JOIN employees_table e ON
 de.emp_no=e.emp_no
+ORDER BY e.emp_no ASC, e.last_name ASC;
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex
