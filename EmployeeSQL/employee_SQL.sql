d1c3c25f890e4a040e25ab238a69d10c9656650f
KG17 <kevingray17@gmail.com>
2020-11-09 09:36:03 -0500

    readded salary

--- EmployeeSQL/employee_SQL.sql
+++ EmployeeSQL/employee_SQL.sql
@@ -1,9 +1,8 @@
 
 -- List the following details of each employee: employee number, last name, first name, sex, and salary.
 
-SELECT e.emp_no, e.last_name, first_name, s.emp_no, sex, FROM salaries_table s
+SELECT e.emp_no, e.last_name, first_name, s.emp_no, sex, s.salary FROM salaries_table s
 JOIN employees_table e ON s.emp_no = e.emp_no;
-
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date FROM employees_table 
